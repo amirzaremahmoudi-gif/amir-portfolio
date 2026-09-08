73d29c65242edf7afd4cbf395d907451bdda7e86
Amir Zare Mahmoudi <amirzaremahmoudi@gmail.com>
2026-09-08 10:43:29 +0330

    Replace Nuxt favicon with AZ branding

--- app/app.vue
+++ app/app.vue
@@ -12,7 +12,12 @@ useHead(() => ({
   htmlAttrs: localeHead.value.htmlAttrs,
   meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }, ...localeHead.value.meta],
   link: [
-    { rel: 'icon', href: '/favicon.ico' },
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'icon', type: 'image/svg+xml', href: '/brand/amir-zare-logo.svg' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'manifest', href: '/site.webmanifest' },
     { rel: 'preload', href: criticalFont.value, as: 'font', type: 'font/woff2', crossorigin: 'anonymous' },
     ...localeHead.value.link
   ]
